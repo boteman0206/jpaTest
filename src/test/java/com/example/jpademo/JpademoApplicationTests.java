48ebccafe185343b7ee9d6c90602b4ed487bfd44
atomu <wei.peng@Marcpoint.com>
2020-08-04 15:24:26 +0800

    测试

--- src/test/java/com/example/jpademo/JpademoApplicationTests.java
+++ src/test/java/com/example/jpademo/JpademoApplicationTests.java
@@ -3,11 +3,18 @@ package com.example.jpademo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
+
 @SpringBootTest
 class JpademoApplicationTests {
 
     @Test
     void contextLoads() {
+        LocalDate date = LocalDate.now();
+        LocalDate localDate = date.plusWeeks(-1);
+        System.out.println(localDate);
+        LocalDate weekFirstDay = date.plusDays(1 - date.getDayOfWeek().getValue());
+        System.out.println(weekFirstDay);
     }
 
 }
